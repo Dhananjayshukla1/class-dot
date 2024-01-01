9217d35c60849890ee96e963983f2a5df1f25e2a
DHANANJAY SHUKLA <dhananjayshukla199@gmail.com>
2024-01-02 04:27:05 +0530

    dr function is now correct for factory function

--- javacript2/index.js
+++ javacript2/index.js
@@ -51,6 +51,7 @@ function rec(lenght,breadth){
         console.log("this is value of lenght and breadth of this object: " + lenght, breadth);
     }
    }
+   
 }
 let a=rec(3,4);
 console.log(a.dr());
