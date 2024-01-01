9336fe4003549dce95e647ccd32e609f7b02e64b
DHANANJAY SHUKLA <dhananjayshukla199@gmail.com>
2024-01-02 04:18:47 +0530

    created object by factory function

--- javacript2/index.js
+++ javacript2/index.js
@@ -1,10 +1,10 @@
 //object cloning by using iteration concept
 //......................................//
 
-let rectangle={
-    lenght:2,
-    breadth:20,
-}
+// let rectangle={
+//     lenght:2,
+//     breadth:20,
+// }
 // let a={};
 // for(let key in rectangle){
 //     console.log(rectangle[key])
@@ -38,3 +38,19 @@ let rectangle={
 //     console.log(c[key],key,key,c[key])
 
 // }
+
+
+
+
+
+function rec(lenght,breadth){
+   return rectangle={
+    lenght:lenght,
+    breadth:breadth,
+    dr: function(){
+        console.log("this is value of lenght and breadth of this object: " + this.lenght, this.breadth);
+    }
+   }
+}
+let a=rec(3,4);
+console.log(a.lenght);
