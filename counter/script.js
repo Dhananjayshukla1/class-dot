96a7df92af36fd4d56ddf1dc0183ca60774f6f17
DHANANJAY SHUKLA <dhananjayshukla199@gmail.com>
2024-01-17 21:27:04 +0530

    n

--- counter/script.js
+++ counter/script.js
@@ -1,16 +1,18 @@
 const vali= document.querySelector('#val');
-
+// vali.innerHTML="value is:";
  function decr(){
   let value=parseInt(vali.innerHTML);
-   value=value-10;
+   value=value-1;
 
    vali.innerHTML=value;
 
 }
 function incr(){
-    let value=parseInt(vali.innerHTML);
-     value=value+10;
+    let value=vali.innerHTML;
+    value=parseInt(value);
+     value=value+1;
   
      vali.innerHTML=value;
   
   }
+//   vali.innerHTML="value is:";
